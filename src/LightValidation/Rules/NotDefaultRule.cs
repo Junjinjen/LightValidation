7b3b4a9aeb4dbfbc43c6b9fe55846a46678c1f2c
Junjinjen <junjinjen@gmail.com>
2026-03-01 01:14:39 +0300

    feat: Add NotDefault rule

---
+++ src/LightValidation/Rules/NotDefaultRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LightValidation.Rules;
+
+public static class NotDefaultRule
+{
+    public static IValidator<TValue, TError> NotDefault<TValue, TError>(this IValidator<TValue, TError> validator, TError error)
+    {
+        ArgumentNullException.ThrowIfNull(validator);
+
+        if (EqualityComparer<TValue>.Default.Equals(validator.Value, default))
+        {
+            validator.AddError(error);
+        }
+
+        return validator;
+    }
+}
